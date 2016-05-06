9de754f0d149fcc16998874b483d109d4ce1e81e
CloudStrife <CloudStrife@CLOUDSTRIFE4E4A>
2016-05-06 10:08:50 -0400

    practice with accessors and random number generator

--- Win32Project1/Win32Project1.cpp
+++ Win32Project1/Win32Project1.cpp
@@ -1,11 +1,42 @@
 // Win32Project1.cpp : Defines the entry point for the console application.
-//
+//Practice file
 
 #include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
 
 
 int main()
 {
+	/*
+	//output to the console using the insertion operator
+	cout << "Test" << endl;
+
+	//input using the extraction operator, assigning the input to a variable
+	cin >> mpg;
+
+	//use static_cast<insert data type here> for explicit conversions
+
+	//Access modifiers:
+	//const
+	
+	//Practice with random number generator
+	srand(time(0));
+	int i = 0;
+	while (i < 6)
+	{
+		int temp = rand() % 6 + 1;
+		cout << temp << '\n';
+		i++;
+	}
+	*/
+
     return 0;
+
+
+
 }
 
